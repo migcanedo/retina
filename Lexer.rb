02ec05ad82cd7046b0d6febcfb69eec7a8c66020
Miguel Canedo <mig_canedo96@hotmail.com>
2017-04-02 23:29:01 -0400

    Proyecto finalizado

--- Lexer.rb
+++ Lexer.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! /usr/bin/ruby
 # Universidad Simon Bolivar
 # Trimestre Enero-Marzo 2017
@@ -74,81 +73,4 @@ class Lexer
 	def to_s
 		(if @errores.empty? then @tokens else @errores end).map { |token| token.to_s }.join "\n"
 	end
-=======
-#! /usr/bin/ruby
-# Universidad Simon Bolivar
-# Trimestre Enero-Marzo 2017
-# Traductores e Interpretadores [CI3725]
-# Rafael Cisneros, 13-11156
-# Miguel Canedo, 13-10214
-# Proyecto, Lexer
-
-require_relative 'Tokens'
-require_relative 'Errores'
-
-# Clase que representa al Lexer.
-class Lexer
-	# Constructor de la clase Lexer
-	def initialize entrada
-		@entrada = entrada # String que contiene el contenido del archivo
-		@linea = 1 # Valor inicial de la fila
-		@columna = 1 # Valor inicial de columna
-		@tokens = [] # Lista de tokens
-		@errores = [] # Lista de errores
-		
-	end
-
-	# Metodo que consume la entrada del lexer y retorna el siguente token que haga match. 
-	# En caso de no existir ningun match, reportara una exception.
-	def obtener_token
-
-		# Se consumen los espacios en blanco y los comentrios.
-		@entrada =~ /\A(\s|\t|\n|\#.*)*/
-		@entrada = $'
-		$&.each_char do |chr|
-			if chr == "\n" # Si es salto de linea, se suma 1 a la linea y se resetea la columna.
-				@linea += 1
-				@columna = 1
-			else
-				@columna += 1
-			end
-		end
-
-		# Si la entrada queda completamente vacia, se retorna nil.
-		if @entrada.empty?
-			return nil
-		end
-
-		# Inicializamos un Error Lexicografico, en caso de no existir ningun match en la entrada.
-		nuevoToken = LexicographError.new(@entrada[0], @linea, @columna)
-
-		# Revisamos si la entrada hace match con algun token.
-		$tokens.each do |token, regex|
-			if @entrada =~ regex
-				tk = Object::const_get("Tk#{token}")
-				nuevoToken = tk.new($&, @linea, @columna)
-				break
-			end
-		end
-
-		# Aumentamos la columna con la longitud del match, o del error, y lo quitamos de la entrada.
-		@columna += nuevoToken.texto.length
-		@entrada = @entrada[nuevoToken.texto.length..@entrada.length-1]
-
-		# Se reporta el error en caso de no existir mathc, o se retorna el token en caso de q si exista un match.
-		if nuevoToken.is_a? LexicographError
-			@errores << nuevoToken
-			raise nuevoToken
-		else
-			@tokens << nuevoToken
-			return nuevoToken
-		end
-	end
-
-	# To String para hacer imprimible al Lexer. Si tiene errores, se imprimen sus errores; pero en caso contrario,
-	# se imprimen los tokens reconocidos.
-	def to_s
-		(if @errores.empty? then @tokens else @errores end).map { |token| token.to_s }.join "\n"
-	end
->>>>>>> 776661ccc006e775a4d59ab38bae03804782468d
 end
--- funcRTN.rb
+++ funcRTN.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #! /usr/bin/ruby
 # Universidad Simon Bolivar
 # Trimestre Enero-Marzo 2017
@@ -13,14 +12,7 @@ require_relative 'SymTable'
 $tablaFunciones = SymTable.new()
 
 # Inicializamos las variables globales necesarias .
-$matrix = [] #Representacion del BitMap.
-for i in 0..1000 
-	m = []
-	for j in 0..1000
-		m << 0
-	end
-	$matrix << m
-end
+$matrix = Array.new(1001) {Array.new(1001, 0)} #Representacion del BitMap.
 $eye = [500,500] # Posicion del cursor de retina.
 $head = 180 	 # Direccion del cursor de retina.
 $status = true	 # Indica si se marcan o no los movimientos del cursor.
@@ -103,7 +95,7 @@ class FuncionBackward
 	def run tabla, param
 		xi = $eye[0]
 		yi = $eye[1]
-		for i in 1..param[0]
+		for i in 1..param[0].round
 			x = xi - (i*Math::cos($head*Math::PI/180)).round
 			y = yi - (i*Math::sin($head*Math::PI/180)).round
 			$matrix[x][y] = 1 if $status && x >= 0 && x < 1001 && y >= 0 && y < 1001
@@ -122,7 +114,7 @@ class FuncionRotatel
 	
 	# Metodo que simula la ejecucion de la funcion.	
 	def run tabla, param
-		$head = ($head + param[0])%360
+		$head = (($head + param[0])%360).round
 	end
 end
 
@@ -136,7 +128,7 @@ class FuncionRotater
 	
 	# Metodo que simula la ejecucion de la funcion.
 	def run tabla, param
-		$head = ($head - param[0])%360
+		$head = (($head - param[0])%360).round
 	end
 end
 
@@ -150,7 +142,7 @@ class FuncionSetPosition
 	
 	# Metodo que simula la ejecucion de la funcion.
 	def run tabla, param
-		$eye = [param[0], param[1]]
+		$eye = [param[0].round + 500, param[1].round + 500]
 		$matrix[param[0]][param[1]] = 1 if $status
 	end
 end
@@ -164,176 +156,3 @@ $tablaFunciones.insert(FuncionBackward.new(), ["number", nil], false)
 $tablaFunciones.insert(FuncionRotatel.new(), ["number", nil], false)
 $tablaFunciones.insert(FuncionRotater.new(), ["number", nil], false)
 $tablaFunciones.insert(FuncionSetPosition.new(), ["number", "number", nil], false)
-
-
-
-=======
-#! /usr/bin/ruby
-# Universidad Simon Bolivar
-# Trimestre Enero-Marzo 2017
-# Traductores e Interpretadores [CI3725]
-# Rafael Cisneros, 13-11156
-# Miguel Canedo, 13-10214
-# Proyecto, Funciones de Retina
-
-require_relative 'SymTable'
-
-# Creamos la Tabla de Simbolos para las funciones
-$tablaFunciones = SymTable.new()
-
-# Inicializamos las variables globales necesarias 
-$matrix = [] #Representacion del BitMap
-for i in 0..1000 
-	m = []
-	for j in 0..1000
-		m << 0
-	end
-	$matrix << m
-end
-$eye = [500,500] # Posicion del cursor de retina
-$head = 180 	 # Direccion del cursor de retina
-$status = true	 # Indica si se marcan o no los movimientos del cursor
-# Marcamos el centro del BitMap
-$matrix[500][500] = 1 
-
-# Clase que representa a la Funcion Home de Retina que coloca el curso en el
-# punto inicial (0,0)
-class FuncionHome
-	attr_reader :idFuncion
-	def initialize
-		
-		@idFuncion = Identificador.new(TkId.new("home", 0, 0))
-	end
-	
-	# Metodo que simula la ejecucion de la funcion
-	def run tabla
-		$eye = [500,500]
-	end
-end
-
-# Clase que representa a la Funcion OpenEye de Retina que indica que se marcaran los 
-# movimientos del cursor a partir de esta instruccion
-class FuncionOpenEye
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("openeye", 0, 0))
-	end
-
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla
-		$status = true
-	end
-end
-
-# Clase que representa a la Funcion CloseEye de Retina que indica que no se marcaran los
-# movimientos del cursor a partir de esta instruccion
-class FuncionCloseEye
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("closeeye", 0, 0))
-	end
-
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla
-		$status = false
-	end
-end
-
-# Clase que representa a la Funcion Forward de Retina que avanza el cursor una cantidad
-# de pasos dada por el parametro
-class FuncionForward
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("forward", 0, 0))
-	end
-
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla, param
-		xi = $eye[0]
-		yi = $eye[1]
-		for i in 1..param[0]
-			x = xi + (i*Math::cos($head*Math::PI/180)).round
-			y = yi + (i*Math::sin($head*Math::PI/180)).round
-			$matrix[x][y] = 1 if $status && x >= 0 && x < 1001 && y >= 0 && y < 1001
-		end
-		$eye = [x,y]
-	end
-end
-
-# Clase que representa a la Funcion Backward de Retina que retrocede el cursor una
-# cantidad de pasos dada por el parametro
-class FuncionBackward
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("backward", 0, 0))
-	end
-	
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla, param
-		xi = $eye[0]
-		yi = $eye[1]
-		for i in 1..param[0]
-			x = xi - (i*Math::cos($head*Math::PI/180)).round
-			y = yi - (i*Math::sin($head*Math::PI/180)).round
-			$matrix[x][y] = 1 if $status && x >= 0 && x < 1001 && y >= 0 && y < 1001
-		end
-		$eye = [x,y]
-	end
-end
-
-# Clase que representa a la Funcion Rotatel de Retina que rota el cursor en sentido
-# antihorario tantos grados como indique el parametro
-class FuncionRotatel
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("rotatel", 0, 0))
-	end
-	
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla, param
-		$head = ($head + param[0])%360
-	end
-end
-
-# Clase que representa a la Funcion Rotater de Retina que rota el cursor en sentido
-# horario tantos grados como indique el parametro
-class FuncionRotater
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("rotater", 0, 0))
-	end
-	
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla, param
-		$head = ($head - param[0])%360
-	end
-end
-
-# Clase que representa a la Funcion SetPosition de Retina que coloca el cursor a la
-# posicion indicada por los parametros
-class FuncionSetPosition
-	attr_reader :idFuncion
-	def initialize
-		@idFuncion = Identificador.new(TkId.new("setposition", 0, 0))
-	end
-	
-	# Metodo que simula la ejecucion de la funcion	
-	def run tabla, param
-		$eye = [param[0], param[1]]
-		$matrix[param[0]][param[1]] = 1 if $status
-	end
-end
-
-# Guardamos todas estas funciones en la tabla global de funciones
-$tablaFunciones.insert(FuncionHome.new(), [nil], false)
-$tablaFunciones.insert(FuncionOpenEye.new(), [nil], false)
-$tablaFunciones.insert(FuncionCloseEye.new(), [nil], false)
-$tablaFunciones.insert(FuncionForward.new(), ["number", nil], false)
-$tablaFunciones.insert(FuncionBackward.new(), ["number", nil], false)
-$tablaFunciones.insert(FuncionRotatel.new(), ["number", nil], false)
-$tablaFunciones.insert(FuncionRotater.new(), ["number", nil], false)
-$tablaFunciones.insert(FuncionSetPosition.new(), ["number", "number", nil], false)
-
-
-
->>>>>>> 776661ccc006e775a4d59ab38bae03804782468d
